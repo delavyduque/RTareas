f7ba3abb453d63043647f98e7ba650ceb8b149f3
Delavy Duque <delavyduque@hotmail.com>
2017-08-13 14:11:28 -0500

    Hecho el número de veces que regresa al origen, y gráficas con diferentes valores de pasos y repeticiones

--- p1.R
+++ p1.R
@@ -1,17 +1,37 @@
-pos <- 0
-maximun <- 0
-dur <- 15
-for (t in 1:dur) {
-  v <- runif(1) 
-  if (v < 0.5) {
-    pos <- pos + 1
-  } else {
-    pos <- pos - 1 
+library(parallel)
+repetir <- 100
+pasos <- 200
+datos <-  data.frame()
+
+experimento <- function(replica) {
+  pos <- rep(0, dimension)
+  origen <- rep(0, dimension)
+  times <-0
+  for (t in 1:pasos) {
+    cambiar <- sample(1:dimension, 1)
+    cambio <- 1
+    if (runif(1) < 0.5) {
+        cambio <- -1
+    }
+    pos[cambiar] <- pos[cambiar] + cambio
+    if (all(pos == origen)) {
+      times <- times + 1
+    }
   }
-  dist <- abs(pos)
-  if (dist > maximun){
-	maximun <- dist
-  }
+  return(times)
 }
 
-  print(maximun)
+cluster <- makeCluster(detectCores() - 1)
+clusterExport(cluster, "pasos")
+
+for (dimension in 1:8) {
+    clusterExport(cluster, "dimension")
+    resultado <- parSapply(cluster, 1:repetir, experimento)
+    datos <- rbind(datos, resultado)
+}
+
+stopCluster(cluster)
+png("p1er100y200.png")
+boxplot(data.matrix(datos), use.cols=FALSE,
+xlab="Dimensi\u{F3}n", ylab="Numero de veces que regresa al origen", main="Euclideana")
+graphics.off()
