ca3e529272fe74ff5eb96582265b7c2d013828f0
DelavyDuque <delavyduque@hotmail.com>
2017-08-21 18:08:31 -0500

    Reto2 completo, sin reporte.

--- p2/reporte.md
+++ p2/reporte.md
@@ -1,2 +1,11 @@
-# Práctica 2
-# Autómata celular
+% Práctica 2
+% Autómata celular
+% Delavy Duque Martínez
+
+# Introducción
+
+Un autómata celular es una colección de células "coloreadas" en una cuadrícula de un tamaño y figura específica, que evoluciona a lo largo del tiempo, mediante pasos, de acuerdo a reglas basadas en los estados de las células vecinas. Las reglas son aplicadas en la cuadrículasiempre y cuando haya una viva, y pueden ser tantos pasos sean necesarios hasta que ya no quede una célula viva. [^1]
+
+En esta práctica, se
+
+[^1]: [](http://mathworld.wolfram.com/CellularAutomaton.html)
--- p2/reto1.R
+++ p2/reto1.R
@@ -3,30 +3,52 @@ suppressMessages(library("sna"))
 unlink("*.png")
 dim <- 50
 num <- dim^2
-probability <- 0.1
 semillas <- 8
-tasa <- 1
+ciclo <-0
 
-paso <- function(pos) {
-    fila <- floor((pos - 1) / dim) + 1
-    columna <- ((pos - 1) %% dim) + 1
-    if (actual[fila, columna] > 0){
-      vecindad <-  actual[max(fila - 1, 1) : min(fila + 1, dim),
-                          max(columna - 1, 1): min(columna + 1, dim)]
-      lugaresCero <- which(vecindad == 0)
-      if(length(lugaresCero) > 0){
-        crecer <- sample(1:length(lugaresCero), tasa)
-        for (i in crecer) {
-          actual[i] <- actual[fila, columna]
-        }
-      }
-      return vecindad
-    }
-}
-
-random <- sample(1:2500, semillas, replace=F)
+random <- sample(1:num, semillas, replace=F)
 actual <- matrix(0, nrow=dim, ncol=dim)
 for (i in 1:semillas) {
   actual[random[i]] <- i/semillas
-  print(actual[random[i]])
 }
+
+paso <- function(pos) {
+  fila <- floor((pos - 1) / dim) + 1
+  columna <- ((pos - 1) %% dim) + 1
+  if (actual[fila, columna] > 0){
+    return(actual[fila, columna])
+  } else {
+    vecindad <- actual[
+      max(fila - 1, 1) : min(fila + 1, dim),
+      max(columna - 1, 1): min(columna + 1, dim)
+    ]
+    lugaresSemilla <- which(vecindad != 0)
+    if(length(lugaresSemilla) > 0){
+      crecer <- sample(lugaresSemilla, 1)
+      return(vecindad[crecer])
+    }else {
+      return(actual[fila, columna])
+    }
+  }
+}
+
+cluster <- makeCluster(detectCores() - 1)
+clusterExport(cluster, "dim")
+
+
+while(any(actual == 0)){
+  clusterExport(cluster, "actual")
+  salida = paste("reto1-paso", ciclo ,".png", sep="")
+  png(salida)
+  plot.sociomatrix(actual, diaglab=F, main=paste("Paso", ciclo))
+  graphics.off()
+  siguiente <- parSapply(cluster, 1:num, paso)
+  actual <- matrix(siguiente, nrow=dim, ncol= dim, byrow= T)
+  ciclo <- ciclo + 1
+}
+actual[1] <-0
+salida = paste("reto1-paso", ciclo ,".png", sep="")
+png(salida)
+plot.sociomatrix(actual, diaglab=F, main=paste("Paso", ciclo))
+graphics.off()
+stopCluster(cluster)
